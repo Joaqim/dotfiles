90816fddbdf11267a6b3640b13dcc0fbd019b3d4
Joaqim Planstedt <git@joaqim.xyz>
2024-07-17 22:46:19 +0200

    fix(pre-commit-hook): disable nixmft, rely on alejandra formatting instead

--- parts/pre-commit.nix
+++ parts/pre-commit.nix
@@ -1,9 +1,14 @@
 {
-  pre-commit.settings.hooks = {
-    alejandra.enable = true;
-    commitizen.enable = true;
-    deadnix.enable = true;
-    nixfmt.enable = true;
-    statix.enable = true;
+  pre-commit = {
+    # For now, don't run check on `nix flake check`,
+    # will activate this after formatting previously
+    # submitted code
+    check.enable = false;
+    settings.hooks = {
+      alejandra.enable = true;
+      commitizen.enable = true;
+      deadnix.enable = true;
+      statix.enable = true;
+    };
   };
 }
