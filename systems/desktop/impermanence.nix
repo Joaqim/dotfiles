b0f610be3bf40296a583962cc7634ed0e2fb486a
Joaqim <mail@joaqim.xyz>
2024-07-18 17:18:34 +0200

    revert(impermanence): For now, no need to selectively persist home folder

--- systems/desktop/impermanence.nix
+++ systems/desktop/impermanence.nix
@@ -16,63 +16,8 @@
       }
     ];
     files = ["/etc/machine-id"];
-
-    users.jq = {
-      directories = [
-        "Downloads"
-        "Music"
-        "Pictures"
-        "Documents"
-        "Videos"
-        "VirtualBox VMs"
-
-        ".mozilla/firefox/Crash Reports"
-        ".mozilla/firefox/Pending Pings"
-        ".mozilla/firefox/jq" # Do this for now, until we enable syncstorage-rs
-
-        ".local/share/direnv"
-        ".local/share/Steam"
-
-        ".local/share/PrismLauncher"
-        ".local/share/Trash"
-
-        ".local/share/baloo"
-        ".local/share/dolphin"
-        ".local/share/kactivitymanagerd"
-        ".local/share/kate"
-        ".local/share/klipper"
-        ".local/share/konsole"
-        ".local/share/kscreen"
-        ".local/share/kwalletd"
-        ".local/share/kxmlgui5"
-        ".local/share/RecentDocuments"
-        ".local/share/sddm"
-        {
-          directory = ".gnupg";
-          mode = "0700";
-        }
-        {
-          directory = ".ssh";
-          mode = "0700";
-        }
-        {
-          directory = ".nixops";
-          mode = "0700";
-        }
-        {
-          directory = ".local/share/keyrings";
-          mode = "0700";
-        }
-      ];
-
-      files = [
-        ".local/share/krunnerstaterc"
-        ".local/share/user-places.xbel"
-        ".local/share/user-places.xbel.bak"
-        ".local/share/user-places.xbel.tbcache"
-      ];
-    };
   };
+
   systemd = {
     tmpfiles = {
       rules = [
