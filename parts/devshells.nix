f848f7c220567496b5c955f4772f6c688f208687
Joaqim <mail@joaqim.xyz>
2024-07-18 16:18:28 +0200

    feat(devshells): Add pkgs: ripgrep and mkpasswd

--- parts/devshells.nix
+++ parts/devshells.nix
@@ -1,14 +1,12 @@
 {
   pkgs,
   config,
-  inputs',
-  self',
   ...
 }: {
   devShells = {
     default = pkgs.mkShell {
       packages = builtins.attrValues {
-        inherit (pkgs) age alejandra just nil sops ssh-to-age;
+        inherit (pkgs) age alejandra just nil sops ssh-to-age ripgrep mkpasswd;
       };
       shellHook = "${config.pre-commit.installationScript}";
     };
