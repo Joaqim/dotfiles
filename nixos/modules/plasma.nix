832cf357c767d7e6b908f72ceea416ac08f783af
Joaqim <mail@joaqim.xyz>
2024-07-18 21:30:33 +0200

    feat(plasma): disable baloo indexing in lieu of usage (?)

--- nixos/modules/plasma.nix
+++ nixos/modules/plasma.nix
@@ -12,19 +12,29 @@
       };
     };
   };
-  environment.plasma5.excludePackages = with pkgs.libsForQt5;
-  # with pkgs.kdePackages;
-    [
-      plasma-browser-integration
-      konsole
-      oxygen
-      dolphin
-      kate
-      gwenview
-      spectacle
-      kdeconnect-kde
-      khelpcenter
-    ];
+
+  environment = {
+    # Disable baloo indexer
+    etc."xdg/baloofilerc".source = (pkgs.formats.ini {}).generate "baloorc" {
+      "Basic Settings" = {
+        "Indexing-Enabled" = false;
+      };
+    };
+    plasma5.excludePackages = with pkgs.libsForQt5;
+    # with pkgs.kdePackages;
+      [
+        plasma-browser-integration
+        konsole
+        oxygen
+        dolphin
+        kate
+        gwenview
+        spectacle
+        kdeconnect-kde
+        khelpcenter
+        baloo
+      ];
+  };
   # qt = {
   #   enable = true;
   #   platformTheme = "gnome";
