95d099fe15a295640bf58741c815d17bc5e5cab5
Joaqim <mail@joaqim.xyz>
2024-07-19 18:40:43 +0200

    feat(doas): Finalize nixosModule

--- nixos/modules/doas.nix
+++ nixos/modules/doas.nix
@@ -1,15 +1,26 @@
-{flake, ...}: {
+{
+  flake,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = [pkgs.doas-sudo-shim];
   security = {
+    sudo.enable = false;
     doas = {
       enable = true;
+
+      extraConfig = ''
+        permit nopass :wheel as root cmd /usr/bin/liquidctl
+      '';
+
       extraRules = [
         {
           keepEnv = true;
           noPass = true;
           users = [flake.config.people.user0];
+          persist = true;
         }
       ];
     };
-    # sudo.enable = false;
   };
 }
