3725712cb1d4c5c140f262fe60115edf5b878983
Joaqim <mail@joaqim.xyz>
2024-07-21 20:00:31 +0200

    feat(vscode): Extensive initial configuration

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -3,37 +3,113 @@
   lib,
   ...
 }: {
+  home.packages = with pkgs; [
+    # Nix Language
+    alejandra
+  ];
+
   programs = {
     vscode = {
       enable = true;
-      package = pkgs.vscode;
-      extensions = with pkgs.vscode-extensions; [
-        eamodio.gitlens
-        myriad-dreamin.tinymist
-        nvarner.typst-lsp
-        catppuccin.catppuccin-vsc
-        james-yu.latex-workshop
-        jnoortheen.nix-ide
-        kamadorueda.alejandra
-        mkhl.direnv
-        pkief.material-icon-theme
-        streetsidesoftware.code-spell-checker
-        tamasfe.even-better-toml
-        esbenp.prettier-vscode
-        mattn.lisp
-      ];
       userSettings = {
+        ##### VsCode Settings #####
+        ## Commonly Used
+        "files.autoSave" = "onFocusChange";
+        "editor.fontSize" = 14;
+        "editor.tabSize" = 4;
+        "editor.renderWhitespace" = "selection";
+        "editor.cursorStyle" = "line";
+        "editor.multiCursorModifier" = "alt";
+        "editor.insertSpaces" = true;
+        "editor.wordWrap" = "off";
+        "files.exclude" = {
+          "**/.git" = true;
+          "**/.svn" = true;
+          "**/.hg" = true;
+          "**/CVS" = true;
+          "**/.DS_Store" = true;
+          "**/Thumbs.db" = true;
+        };
+        "files.associations" = {};
+
+        # Git
         "git.confirmSync" = false;
-        "editor.insertSpaces" = false;
-        "files.autoSave" = "afterDelay";
         "git.enableSmartCommit" = true;
-        "nix.enableLanguageServer" = true;
-        "nix.serverPath" = lib.getExe pkgs.nil;
-        "nix.formatterPath" = lib.getExe pkgs.alejandra;
-        "window.menuBarVisibility" = "toggle";
+
+        ## Workbench
         "workbench.iconTheme" = "material-icon-theme";
         "workbench.startupEditor" = "none";
         "workbench.colorTheme" = "Catppuccin Mocha";
+        "workbench.editor.enablePreview" = true;
+
+        ## Window
+        "window.autoDetectColorScheme" = false;
+        "window.menuBarVisibility" = "toggle";
+
+        ## Features
+        #### Explorer
+        "explorer.confirmDelete" = false;
+        "explorer.confirmDragAndDrop" = false;
+        "explorer.enableDragAndDrop" = false;
+
+        ## Application
+        #### Update
+        "update.mode" = "none";
+        "update.showReleaseNotes" = false;
+
+        ## Extensions
+        #### Material Icon Theme
+        "material-icon-theme.files.associations" = {
+          "*.ts" = "typescript";
+          "**.json" = "json";
+          "filename.tsx" = "react";
+          "fileName.ts" = "typescript";
+        };
+        "material-icon-theme.folders.associations" = {
+          "applications" = "folder-apps";
+          "home-manager" = "folder-home";
+          "laptop" = "folder-desktop";
+          "MinecraftModpack" = "Minecraft";
+          "nas" = "Context";
+          "nixos" = "Projects";
+          "parts" = "folder-components";
+          "systems" = "Decorators";
+          "user0" = "Guard";
+          "user1" = "Private";
+          "user2" = "Private";
+          "work" = "folder-desktop";
+        };
+
+        ### Nix Language
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = lib.getExe pkgs.nil;
+        "nix.formatterPath" = lib.getExe pkgs.alejandra;
+      };
+      extensions = with pkgs.vscode-extensions; [
+        catppuccin.catppuccin-vsc
+        codeium.codeium
+        eamodio.gitlens
+        esbenp.prettier-vscode
+        evolution-gaming.evolution-gaming--vscode-eslint # ESLint 'autoFixOnSave'
+        james-yu.latex-workshop
+        jnoortheen.nix-ide
+        kamadorueda.alejandra
+        marp-team.marp-vscode
+        mattn.lisp
+        mkhl.direnv
+        myriad-dreamin.tinymist
+        nvarner.typst-lsp
+        oavbls.pretty-ts-errors
+        pkief.material-icon-theme
+        signageos.signageos-vscode-sops
+        signageos.signageos-vscode-sops
+        streetsidesoftware.code-spell-checker
+        tamasfe.even-better-toml
+        thenuprojectcontributors.vscode-nushell-lang
+        wakatime.vscode-wakatime
+        yzhang.markdown-all-in-one
+      ];
+      userSettings = {
       };
     };
   };
