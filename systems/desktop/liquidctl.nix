29df231beb70554d56294e05c1178d28635ba9bd
Joaqim <mail@joaqim.xyz>
2024-07-21 21:22:25 +0200

    fix(liquidctl): Allow ExecStart to fail when no Liquidctl hardware is presumably available

--- systems/desktop/liquidctl.nix
+++ systems/desktop/liquidctl.nix
@@ -8,18 +8,18 @@
         ## AIO water cooler
 
         # set pump speed
-        "${pkgs.liquidctl}/bin/liquidctl --match Hydro initialize --pump-mode performance"
+        "-${pkgs.liquidctl}/bin/liquidctl --match Hydro initialize --pump-mode performance"
         # fan curve
-        "${pkgs.liquidctl}/bin/liquidctl --match Hydro set fan speed 20 50 34 80 90 100"
+        "-${pkgs.liquidctl}/bin/liquidctl --match Hydro set fan speed 20 50 34 80 90 100"
         # turn off RGB
-        "${pkgs.liquidctl}/bin/liquidctl --match Hydro set sync color fixed 000000"
+        "-${pkgs.liquidctl}/bin/liquidctl --match Hydro set sync color fixed 000000"
 
         ## Motherboard
 
         # Initialize
-        "${pkgs.liquidctl}/bin/liquidctl --match Fusion initialize"
+        "-${pkgs.liquidctl}/bin/liquidctl --match Fusion initialize"
         # turn of RGB
-        "${pkgs.liquidctl}/bin/liquidctl --match Fusion set sync color fixed 000000"
+        "-${pkgs.liquidctl}/bin/liquidctl --match Fusion set sync color fixed 000000"
       ];
     };
     wantedBy = ["default.target"];
