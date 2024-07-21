c006537c3ce7540402fe8f47d2a85ac650915186
Joaqim <mail@joaqim.xyz>
2024-07-21 22:55:42 +0200

    fix(pre-commit): Ignore `nvfetcher` generated sources

--- parts/pre-commit.nix
+++ parts/pre-commit.nix
@@ -1,14 +1,25 @@
 {
   pre-commit = {
-    # For now, don't run check on `nix flake check`,
-    # will activate this after formatting previously
-    # submitted code
     check.enable = true;
-    settings.hooks = {
-      alejandra.enable = true;
-      commitizen.enable = true;
-      deadnix.enable = true;
-      statix.enable = true;
+
+    settings.hooks = let
+      # Define shared settings
+      excludes = ["_sources"];
+    in {
+      alejandra = {
+        enable = true;
+        inherit excludes;
+      };
+      commitizen = {
+        enable = true;
+      };
+      deadnix = {
+        enable = true;
+        inherit excludes;
+      };
+      statix = {
+        enable = true;
+      };
     };
   };
 }
