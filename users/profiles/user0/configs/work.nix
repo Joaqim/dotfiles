9f41c09dfd98e3901f280a720d8aabab07d518dd
Joaqim <mail@joaqim.xyz>
2024-07-21 23:42:07 +0200

    chore(homeModules): Disable `entertainment` and `jellyfin` @work

--- users/profiles/user0/configs/work.nix
+++ users/profiles/user0/configs/work.nix
@@ -3,13 +3,12 @@
 in {
   imports = [
     self.homeModules.commandLine
-    self.homeModules.entertainment
     self.homeModules.extras
     self.homeModules.fileManagement
     self.homeModules.firefoxHM
     self.homeModules.gpg
     self.homeModules.internet
-    self.homeModules.jellyfin
+    self.homeModules.mpv
     self.homeModules.privacy
     self.homeModules.productionCode
     self.homeModules.productionWriting
