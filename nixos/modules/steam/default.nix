0bf1b8580b944f24915f514f4fc1715187d8b90a
Joaqim <mail@joaqim.xyz>
2024-07-29 05:55:04 +0200

    feat(steam): Enable `extest` for steam X11->Wayland SteamInput Mapping

--- nixos/modules/steam/default.nix
+++ nixos/modules/steam/default.nix
@@ -2,6 +2,7 @@
   programs = {
     steam = {
       enable = true;
+      extest.enable = true; # X11->Wayland SteamInput mapping
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
       extraPackages = [pkgs.curl];
@@ -9,4 +10,19 @@
     java.enable = true;
   };
   hardware.steam-hardware.enable = true;
+  # Since extest fixes the keyboard on Wayland, we probably want autostart for Steam
+  environment.systemPackages = [
+    (pkgs.makeAutostartItem rec {
+      name = "steam";
+      package = pkgs.makeDesktopItem {
+        inherit name;
+        desktopName = "Steam";
+        exec = "steam -silent %U";
+        icon = "steam";
+        extraConfig = {
+          OnlyShowIn = "KDE";
+        };
+      };
+    })
+  ];
 }
