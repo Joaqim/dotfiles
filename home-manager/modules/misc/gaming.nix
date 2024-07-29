af3be7c174f0b4d18a145ebdd14704caef80cc38
Joaqim <mail@joaqim.xyz>
2024-07-29 06:06:21 +0200

    fix(gaming): Re-add proper config for additions to `home.packages`

--- home-manager/modules/misc/gaming.nix
+++ home-manager/modules/misc/gaming.nix
@@ -2,21 +2,22 @@
   pkgs,
   nur,
   ...
-}:
-builtins.attrValues {
-  inherit
-    (pkgs)
-    grapejuice
-    heroic
-    lutris
-    mangohud
-    prismlauncher
-    protonup-qt
-    steam
-    steamtinkerlaunch
-    ;
-  inherit
-    (nur.repos.jpyke3)
-    suyu-dev
-    ;
+}: {
+  home.packages = builtins.attrValues {
+    inherit
+      (pkgs)
+      grapejuice
+      heroic
+      lutris
+      mangohud
+      prismlauncher
+      protonup-qt
+      steam
+      steamtinkerlaunch
+      ;
+    inherit
+      (nur.repos.jpyke3)
+      suyu-dev
+      ;
+  };
 }
