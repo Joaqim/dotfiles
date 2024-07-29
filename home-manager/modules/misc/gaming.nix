46c6c531b5c3ab26d47945454c12f5fb520393e1
Joaqim <mail@joaqim.xyz>
2024-07-29 05:56:24 +0200

    feat(gaming): Add additional tools and dependencies for gaming

--- home-manager/modules/misc/gaming.nix
+++ home-manager/modules/misc/gaming.nix
@@ -1,12 +1,22 @@
-{pkgs, ...}: {
-  home.packages = builtins.attrValues {
-    inherit
-      (pkgs)
-      heroic
-      lutris
-      prismlauncher
-      steam
-      grapejuice
-      ;
-  };
+{
+  pkgs,
+  nur,
+  ...
+}:
+builtins.attrValues {
+  inherit
+    (pkgs)
+    grapejuice
+    heroic
+    lutris
+    mangohud
+    prismlauncher
+    protonup-qt
+    steam
+    steamtinkerlaunch
+    ;
+  inherit
+    (nur.repos.jpyke3)
+    suyu-dev
+    ;
 }
