1987ca994e526a87f5d40a8426d7d7d98e050a25
Joaqim <mail@joaqim.xyz>
2024-07-31 20:51:44 +0200

    fix(atuin): Manually enable atuin daemon for improved zfs write performance

--- home-manager/modules/atuin.nix
+++ home-manager/modules/atuin.nix
@@ -1,10 +1,23 @@
-{
+{pkgs, ...}: {
   programs.atuin = {
     enable = true;
     enableNushellIntegration = true;
     flags = ["--disable-up-arrow"];
     settings = {
+      daemon.enabled = true;
       sync_address = "http://desktop.joaqim.com:8888";
+    };
+  };
+
+  systemd.user.services = {
+    "atuind" = {
+      Service = {
+        Environment = "ATUIN_LOG=\"info\"";
+        ExecStart = "${pkgs.atuin}/bin/atuin daemon";
+      };
+      Install = {
+        WantedBy = ["default.target"];
+      };
     };
   };
 }
