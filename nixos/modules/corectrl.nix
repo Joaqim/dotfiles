379a76ffa0b6fce8a7a9869817f6e4dc605b6ab3
Joaqim <mail@joaqim.xyz>
2024-08-02 14:33:21 +0200

    feat(corectrl): Add autostart with sudo polkitd permission for `corectrl`

--- nixos/modules/corectrl.nix
+++ nixos/modules/corectrl.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   programs.corectrl = {
     enable = true;
     gpuOverclock = {
@@ -6,4 +6,28 @@
       ppfeaturemask = "0xffffffff";
     };
   };
+
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+        if ((action.id == "org.corectrl.helper.init" ||
+            action.id == "org.corectrl.helperkiller.init") &&
+            subject.local == true &&
+            subject.active == true &&
+            subject.isInGroup("wheel")) {
+                return polkit.Result.YES;
+        }
+    });
+  '';
+
+  environment.systemPackages = [
+    (pkgs.makeAutostartItem rec {
+      name = "corectl";
+      package = pkgs.makeDesktopItem {
+        inherit name;
+        desktopName = "CoreCtrl";
+        exec = "corectrl --minimize-systray";
+        icon = "corectrl";
+      };
+    })
+  ];
 }
