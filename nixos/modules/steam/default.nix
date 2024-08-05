9931a2d3cbcec869aa4785e7e299ba73f5176cf9
Joaqim <mail@joaqim.xyz>
2024-08-05 18:02:26 +0200

    feat(gaming): Add som Wine/Proton system dependencies.

--- nixos/modules/steam/default.nix
+++ nixos/modules/steam/default.nix
@@ -11,18 +11,29 @@
   };
   hardware.steam-hardware.enable = true;
   # Since extest fixes the keyboard on Wayland, we probably want autostart for Steam
-  environment.systemPackages = [
-    (pkgs.makeAutostartItem rec {
-      name = "steam";
-      package = pkgs.makeDesktopItem {
-        inherit name;
-        desktopName = "Steam";
-        exec = "steam -silent %U";
-        icon = "steam";
-        extraConfig = {
-          OnlyShowIn = "KDE";
+  environment.systemPackages =
+    [
+      (pkgs.makeAutostartItem rec {
+        name = "steam";
+        package = pkgs.makeDesktopItem {
+          inherit name;
+          desktopName = "Steam";
+          exec = "steam -silent %U";
+          icon = "steam";
+          extraConfig = {
+            OnlyShowIn = "KDE";
+          };
         };
-      };
-    })
-  ];
+      })
+    ]
+    ++ builtins.attrValues {
+      # Dependencies used by some Wine/Proton emulation
+      inherit
+        (pkgs.gst_all_1)
+        gst-plugins-bad
+        gst-plugins-base
+        gst-plugins-good
+        gst-plugins-ugly
+        ;
+    };
 }
