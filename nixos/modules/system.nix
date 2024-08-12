c3db1b7ef4b9434f762bb85d536de724ba73dcee
Joaqim <mail@joaqim.xyz>
2024-08-12 14:19:34 +0200

    feat(caddy): Allow caddy executable to use ports <1024

--- nixos/modules/system.nix
+++ nixos/modules/system.nix
@@ -1,1 +1,9 @@
-_: {}
+{pkgs, ...}: {
+  # This lets Caddy bind to ports: <1024
+  security.wrappers."caddy" = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_net_bind_service=+ep";
+    source = "${pkgs.caddy}/bin/caddy";
+  };
+}
