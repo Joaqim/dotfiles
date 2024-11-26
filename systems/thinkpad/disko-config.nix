1c06faecf19bd8e20e52194964cfa3812e7576c0
Joaqim <mail@joaqim.xyz>
2024-11-26 15:57:53 +0100

    fix(disko): Configure `disko` config @ `thinkpad`

--- systems/thinkpad/disko-config.nix
+++ systems/thinkpad/disko-config.nix
@@ -4,20 +4,28 @@
       main = {
         type = "disk";
         # When using disko-install, we will overwrite this value from the commandline
-        #device = "/dev/sda";
+        device = "/dev/sdc";
         content = {
           type = "gpt";
           partitions = {
             ESP = {
               priority = 1;
               name = "ESP";
-              start = "1M";
-              end = "128M";
+              end = "64M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
               };
             };
             swap = {
@@ -26,15 +34,6 @@
                 type = "swap";
                 discardPolicy = "both";
                 resumeDevice = true; # Resume from hibernation from this device
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4"; # Override existing partition
-                mountpoint = "/";
-                mountOptions = ["noatime"];
               };
             };
           };
