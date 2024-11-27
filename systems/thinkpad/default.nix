d81e1146d0d85e968c4917da6e0b37acb2b318e1
Joaqim <mail@joaqim.xyz>
2024-11-27 12:08:56 +0100

    feat(thinkpad): Enable headless always-on for laptop

--- systems/thinkpad/default.nix
+++ systems/thinkpad/default.nix
@@ -6,6 +6,13 @@
     ./networking.nix
     ./ssh.nix
   ];
+  # Switch off screen after 5 minutes of inactivity
+  boot.kernelParams = [
+    "consoleblank=300"
+  ];
+  # Don't turn off laptop when lid is closed while connected to external power
+  services.logind.lidSwitchExternalPower = "ignore";
+
   sops.gnupg.home = lib.mkForce "/var/lib/sops";
 
   i18n.defaultLocale = lib.mkForce "sv_SE.UTF-8";
