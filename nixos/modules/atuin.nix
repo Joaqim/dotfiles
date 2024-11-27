3767e18d179331e543efba3ff406375c180d51a9
Joaqim <mail@joaqim.xyz>
2024-11-27 13:24:49 +0100

    fix(atuin): Revert invalid external hostname for locally running tailscale-only accessible atuin server

--- nixos/modules/atuin.nix
+++ nixos/modules/atuin.nix
@@ -1,8 +1,8 @@
 {
   services.atuin = {
     enable = true;
-
-    host = "desktop";
+    # Ensures we only have access through tailscale node, even if the default port `8888` happens to be open.
+    host = "0.0.0.0";
     openRegistration = false;
   };
 }
