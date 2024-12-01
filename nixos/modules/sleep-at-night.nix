48c8117cc35c4e8e326ff8de3636ba3a0078057c
Joaqim <mail@joaqim.xyz>
2024-12-01 09:27:02 +0100

    feat(nixosModules): Update `sleep-at-night`

--- nixos/modules/sleep-at-night.nix
+++ nixos/modules/sleep-at-night.nix
@@ -7,24 +7,43 @@
 # https://www.reddit.com/r/NixOS/comments/kn1pvj/comment/ghif5jg
 let
   cfg = config.services.sleep-at-night;
-  sleep-if-night = pkgs.writeScriptBin "sleep-if-night" ''
-    #!${lib.getExe pkgs.bash}
-    wakeupTime="$3"
-    shutdownHour="00$1"
-    shutdownHour="''${shutdownHour:(-2)}"
-    shutdownMinute="00$2"
-    shutdownMinute="''${shutdownMinute:(-2)}"
-    currentHour="$(${pkgs.coreutils}/bin/date +%H)"
-    currentMinute="$(${pkgs.coreutils}/bin/date +%M)"
-    if [[ "$currentHour" -eq "$shutdownHour" ]] && [[ "$currentMinute" -eq "$shutdownMinute" ]]
-    then
-        echo "Shutting down now. Waking up at $wakeupTime".
-        ${pkgs.utillinux}/bin/rtcwake -m off -t "$(${pkgs.coreutils}/bin/date -d "$wakeupTime" +%s)";
-    else
-        echo "Current time is $currentHour:$currentMinute. Shutting down at $shutdownHour:$shutdownMinute."
-        exit 0
-    fi
-  '';
+  sleep-at-night = let
+    date = lib.getExe' pkgs.coreutils "date";
+    rtcwake = lib.getExe' pkgs.utillinux "rtcwake";
+  in
+    pkgs.writeScriptBin "sleep-at-night" ''
+      #!${lib.getExe pkgs.bash}
+      set -e
+      wakeupTime="$3"
+      shutdownHour="00$1"
+      shutdownHour="''${shutdownHour:(-2)}"
+      shutdownMinute="00$2"
+      shutdownMinute="''${shutdownMinute:(-2)}"
+      currentHour="$(${date} +%H)"
+      currentMinute="$(${date} +%M)"
+
+      # Calculate the duration until the shutdown time, ''${number#0} removes leading zeros, 09 -> 9
+      currentSeconds=$(( ''${currentHour#0} * 3600 + ''${currentMinute#0} * 60 ))
+      shutdownSeconds=$(( ''${shutdownHour#0} * 3600 + ''${shutdownMinute#0} * 60 ))
+      duration=$(( shutdownSeconds - currentSeconds ))
+
+      # If the duration is negative, the shutdown time is assumed to fall into the next day
+      if [ $duration -lt 0 ]; then
+        echo "Current time is $currentHour:$currentMinute. Planning shutdown tomorrow at $shutdownHour:$shutdownMinute."
+        duration=$(( 24 * 3600 - currentSeconds + shutdownSeconds ))
+      else
+        echo "Current time is $currentHour:$currentMinute. Planning shutdown at $shutdownHour:$shutdownMinute."
+      fi
+
+      # Sleep for the duration
+      if [ $duration -gt 0 ]; then
+        echo "Sleeping for $(${date} -d "@$(( duration ))" -u '+%-H hours and %-M minutes')"
+        sleep $(( duration ))
+      fi
+
+      echo "Shutting down now. Waking up at $wakeupTime".
+      ${rtcwake} -m off -t "$(${date} -d "$wakeupTime" +%s)";
+    '';
 in
   with lib; {
     options = {
@@ -70,7 +89,7 @@ in
       systemd.services.sleep-at-night = {
         description = "Sleep at night.";
         serviceConfig = {
-          ExecStart = "${sleep-if-night}/bin/sleep-if-night ${toString cfg.shutdown.hour} ${toString cfg.shutdown.minute} ${cfg.wakeup}";
+          ExecStart = "${sleep-at-night}/bin/sleep-at-night ${toString cfg.shutdown.hour} ${toString cfg.shutdown.minute} ${cfg.wakeup}";
           Restart = "on-success";
           RestartSec = 30;
           User = "root";
