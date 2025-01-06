01d39ffc5bcfec548ee4ba8758ba84bfde720fd2
Joaqim Planstedt <mail@joaqim.xyz>
2025-01-06 22:57:54 +0100

    feat(nixos): Update nixos state 24.05 -> 24.11 @ `desktop`

--- systems/desktop/default.nix
+++ systems/desktop/default.nix
@@ -11,14 +11,5 @@
     ./kernel-optimization.nix
   ];
   nixpkgs.hostPlatform = lib.mkForce "x86_64-linux";
-  system.stateVersion = lib.mkForce "24.05";
-
-  # When booting into emergency or rescue targets, do not require the password
-  # of the root user to start a root shell.  I am ok with the security
-  # consequences, for this host.  Do not blindly copy this without
-  # understanding.  Note that SYSTEMD_SULOGIN_FORCE is considered semi-unstable
-  systemd.services = {
-    emergency.environment.SYSTEMD_SULOGIN_FORCE = "1";
-    rescue.environment.SYSTEMD_SULOGIN_FORCE = "1";
-  };
+  system.stateVersion = lib.mkForce "24.11";
 }
