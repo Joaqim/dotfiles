e103aa264803d7d7339ab2c7d494df10a24bc512
Joaqim Planstedt <mail@joaqim.xyz>
2025-01-16 04:50:34 +0100

    fix(cataclysm-dda): Remove superfluous hash for git source override

--- home-manager/modules/games/cataclysm-dda.nix
+++ home-manager/modules/games/cataclysm-dda.nix
@@ -3,7 +3,6 @@
     cataclysm-dda-git = pkgs.cataclysm-dda-git.override {
       version = "0.H-RELEASE"; # 2024-10-24
       rev = "08f04fd07f028219a65f2f1fc8e73719dc9c954e";
-      sha256 = "sha256-0s5fzr8zmlpgksim2alz88sx61jfhymkd9zms5p1sci16g8q1gcz";
     };
   };
 }
