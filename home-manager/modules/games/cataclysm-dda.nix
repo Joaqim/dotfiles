4e13dcc291a8d213891e392e010b0bd58ec94714
Joaqim Planstedt <mail@joaqim.xyz>
2025-01-28 19:10:02 +0100

    fix(cataclysm-dda): Use home directory for local configurations

--- home-manager/modules/games/cataclysm-dda.nix
+++ home-manager/modules/games/cataclysm-dda.nix
@@ -3,6 +3,7 @@
     cataclysm-dda-git = pkgs.cataclysm-dda-git.override {
       version = "0.H-RELEASE"; # 2024-10-24
       rev = "08f04fd07f028219a65f2f1fc8e73719dc9c954e";
+      useXdgDir = true;
     };
   };
 }
