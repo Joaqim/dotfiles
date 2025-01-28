ff0e5b312f0bb1268d8c6bb6a9331e6ea1a1c8be
Joaqim Planstedt <mail@joaqim.xyz>
2025-01-28 19:10:43 +0100

    fix(nix): Allow unfree for `unrar`

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -1,7 +1,11 @@
-{
+{lib, ...}: {
   nixpkgs = {
     config = {
       allowUnfree = true;
+      allowUnfreePredicate = pkg:
+        builtins.elem (lib.getName pkg) [
+          "unrar"
+        ];
       permittedInsecurePackages = [
         "electron"
       ];
