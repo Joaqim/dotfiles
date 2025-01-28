7fca5c87bd83900c85caa8d7305e34e12c774fd0
Joaqim Planstedt <mail@joaqim.xyz>
2025-01-28 19:13:11 +0100

    fix(users): Use full github name for `user0`

--- users/config.nix
+++ users/config.nix
@@ -9,7 +9,7 @@ in {
   users = {
     mutableUsers = false;
     "${user0}" = {
-      name = "Joaqim";
+      name = "Joaqim Planstedt";
       email = "mail@joaqim.xyz";
       sshKeys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK58SYMpYN5W9x8tt7gBoGT8bSOFVagSWxJsD4wPU5Z1 mail@joaqim.xyz"
