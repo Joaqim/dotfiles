ac4da13b3b48ff54d48932d82ef5477f9d43d17b
Joaqim Planstedt <mail@joaqim.xyz>
2025-02-19 21:33:12 +0100

    fix(atuin): Use correct `daemon.enable` setting for home manager

--- home-manager/modules/atuin.nix
+++ home-manager/modules/atuin.nix
@@ -1,30 +1,12 @@
 {
-  pkgs,
-  lib,
-  ...
-}: {
   programs.atuin = {
     enable = true;
     enableBashIntegration = true;
     enableNushellIntegration = true;
     flags = ["--disable-up-arrow"];
+    daemon.enable = true;
     settings = {
-      daemon.enabled = true;
-      sync_address = "http://desktop.joaqim.com:8888";
-    };
-  };
-
-  systemd.user.services = {
-    "atuind" = let
-      atuin = lib.getExe pkgs.atuin;
-    in {
-      Service = {
-        Environment = "ATUIN_LOG=\"info\"";
-        ExecStart = "${atuin} daemon";
-      };
-      Install = {
-        WantedBy = ["default.target"];
-      };
+      sync_address = "http://desktop:8888";
     };
   };
 }
