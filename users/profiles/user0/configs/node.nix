21cf9772615f8ddc15d98cb57e1a6953a01ebbda
Joaqim Planstedt <mail@joaqim.xyz>
2025-02-25 06:48:14 +0100

    fix(users): Add `sops` modules to home manager for jq@node

--- users/profiles/user0/configs/node.nix
+++ users/profiles/user0/configs/node.nix
@@ -2,11 +2,13 @@
   inherit (flake) self;
 in {
   imports = [
+    self.inputs.sops-nix.homeManagerModules.sops
     self.homeModules.commandLine
     self.homeModules.internet
     self.homeModules.minecraft-server-toggle
     self.homeModules.misc-gaming
     self.homeModules.themes
     self.homeModules.syncthing
+    self.homeModules.sops
   ];
 }
