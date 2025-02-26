19ccd61667cfd3ad9795ca21bb38c5a91941cd88
Joaqim Planstedt <mail@joaqim.xyz>
2025-02-26 14:15:57 +0100

    fix(themes): Override postInstall for incorrect src path for qt5ct/cattppuccin theme conf

--- home-manager/modules/misc/themes.nix
+++ home-manager/modules/misc/themes.nix
@@ -1,10 +1,14 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  catppuccin = pkgs.catppuccin.overrideAttrs ({installPhase, ...}: {
+    installPhase = builtins.replaceStrings ["qt5ct/themes/Catppuccin-"] ["qt5ct/themes/catppuccin-"] installPhase;
+  });
+in {
   home.packages = builtins.attrValues {
     inherit
       (pkgs)
-      catppuccin
       catppuccin-gtk
       openrgb
       ;
+    inherit catppuccin;
   };
 }
