1985437ab810d62f77f5bdcd5b94847113aa560e
Joaqim Planstedt <mail@joaqim.xyz>
2025-02-27 06:12:39 +0100

    fix(plasmoids): Update derivation for `twitchindicator`

--- pkgs/plugins/plasmoids/twitchindicator/default.nix
+++ pkgs/plugins/plasmoids/twitchindicator/default.nix
@@ -1,14 +1,25 @@
-{pkgs, ...}:
-pkgs.stdenvNoCC.mkDerivation {
-  pname = "twitchindicator";
-  version = "1.0.0";
-  src = pkgs.fetchFromGitHub {
-    owner = "kuunha";
-    repo = "twitchindicator";
-  };
+{
+  stdenvNoCC,
+  sources,
+  pkgs,
+  ...
+}:
+stdenvNoCC.mkDerivation {
+  inherit (sources.twitchindicator) pname version src;
+  dontBuild = true;
+  dontUnpack = true;
+
+  nativeBuildInputs = with pkgs; [
+    kdePackages.wrapQtAppsHook
+  ];
+
+  runtimeDependencies = with pkgs.libsForQt5.qt5; [
+    qtgraphicaleffects
+    qtwebengine
+  ];
 
   installPhase = ''
-    mkdir -p $out/usr/share/plasma/plasmoids/
-    cp -r $src $out/org.github.kuunha.twitchindicator
+    mkdir -p $out/share/plasma/plasmoids/
+    cp -r $src/package $out/share/plasma/plasmoids/org.github.komorebi.twitchindicator
   '';
 }
