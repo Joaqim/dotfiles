d8cdb40d5e199d634e749a4999d8f43b357e2c9f
Joaqim Planstedt <mail@joaqim.xyz>
2025-03-04 10:28:28 +0100

    feat(git): Add option: `my.home.git.lazygit`

--- modules/home/git/default.nix
+++ modules/home/git/default.nix
@@ -23,6 +23,8 @@ in {
       example = "Alice Cooper";
       description = "descriptive name used by git";
     };
+
+    lazygit = my.mkDisableOption "enable lazygit ui";
   };
 
   config.home.packages = with pkgs;
@@ -32,165 +34,168 @@ in {
       tig
     ];
 
-  config.programs.git = lib.mkIf cfg.enable {
-    enable = true;
-
-    inherit (cfg) package userEmail userName;
-
-    signing = {
-      key = lib.mkDefault null;
-      signByDefault = true;
-      format = "ssh";
-    };
-
-    aliases = {
-      git = "!git";
-      lol = "log --graph --decorate --pretty=oneline --abbrev-commit --topo-order";
-      lola = "lol --all";
-      assume = "update-index --assume-unchanged";
-      unassume = "update-index --no-assume-unchanged";
-      assumed = "!git ls-files -v | grep ^h | cut -c 3-";
-      pick = "log -p -G";
-      push-new =
-        "!git push -u origin "
-        + ''"$(git branch | grep '^* ' | cut -f2- -d' ')"'';
-      root = "git rev-parse --show-toplevel";
-    };
-
-    lfs.enable = true;
-
-    delta = {
+  config.programs = lib.mkIf cfg.enable {
+    git = {
       enable = true;
 
-      options = {
-        features = "diff-highlight decorations";
+      inherit (cfg) package userEmail userName;
 
-        # Less jarring style for `diff-highlight` emulation
-        diff-highlight = {
-          minus-style = "red";
-          minus-non-emph-style = "red";
-          minus-emph-style = "bold red 52";
+      signing = {
+        key = lib.mkDefault null;
+        signByDefault = true;
+        format = "ssh";
+      };
 
-          plus-style = "green";
-          plus-non-emph-style = "green";
-          plus-emph-style = "bold green 22";
+      aliases = {
+        git = "!git";
+        lol = "log --graph --decorate --pretty=oneline --abbrev-commit --topo-order";
+        lola = "lol --all";
+        assume = "update-index --assume-unchanged";
+        unassume = "update-index --no-assume-unchanged";
+        assumed = "!git ls-files -v | grep ^h | cut -c 3-";
+        pick = "log -p -G";
+        push-new =
+          "!git push -u origin "
+          + ''"$(git branch | grep '^* ' | cut -f2- -d' ')"'';
+        root = "git rev-parse --show-toplevel";
+      };
 
-          whitespace-error-style = "reverse red";
-        };
+      lfs.enable = true;
 
-        # Personal preference for easier reading
-        decorations = {
-          commit-style = "raw"; # Do not recolor meta information
-          keep-plus-minus-markers = true;
-          paging = "always";
+      delta = {
+        enable = true;
+
+        options = {
+          features = "diff-highlight decorations";
+
+          # Less jarring style for `diff-highlight` emulation
+          diff-highlight = {
+            minus-style = "red";
+            minus-non-emph-style = "red";
+            minus-emph-style = "bold red 52";
+
+            plus-style = "green";
+            plus-non-emph-style = "green";
+            plus-emph-style = "bold green 22";
+
+            whitespace-error-style = "reverse red";
+          };
+
+          # Personal preference for easier reading
+          decorations = {
+            commit-style = "raw"; # Do not recolor meta information
+            keep-plus-minus-markers = true;
+            paging = "always";
+          };
         };
       };
+
+      # There's more
+      extraConfig = {
+        # Makes it a bit more readable
+        blame = {
+          coloring = "repeatedLines";
+          markIgnoredLines = true;
+          markUnblamables = true;
+        };
+
+        # I want `pull --rebase` as a default
+        branch = {
+          autoSetupRebase = "always";
+        };
+
+        checkout = {
+          defaultRemote = "origin";
+        };
+
+        # Shiny colors
+        color = {
+          branch = "auto";
+          diff = "auto";
+          interactive = "auto";
+          status = "auto";
+          ui = "auto";
+        };
+
+        # Pretty much the usual diff colors
+        "color.diff" = {
+          commit = "yellow";
+          frag = "cyan";
+          meta = "yellow";
+          new = "green";
+          old = "red";
+          whitespace = "red reverse";
+        };
+
+        commit = {
+          # Show my changes when writing the message
+          verbose = true;
+          # We will sign using ssh key
+          gpgsign = true;
+        };
+
+        diff = {
+          # Usually leads to better results
+          algorithm = "patience";
+        };
+
+        fetch = {
+          # I don't want hanging references
+          prune = true;
+          pruneTags = true;
+        };
+
+        gpg = {
+          ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+        };
+
+        init = {
+          defaultBranch = "main";
+        };
+
+        merge = {
+          conflictStyle = "zdiff3";
+        };
+
+        pull = {
+          # Avoid useless merge commits
+          rebase = true;
+          ff = "only";
+        };
+
+        push = {
+          default = "simple";
+        };
+
+        rebase = {
+          autoSquash = true;
+          autoStash = true;
+        };
+
+        rerere = {
+          enabled = true;
+        };
+        # Explicitly use ssh key for signing
+        user = {
+          signingkey = "~/.ssh/id_ed25519.pub";
+        };
+
+        url = {
+          "ssh://git@" = {
+            pushInsteadOf = "https://";
+          };
+        };
+      };
+
+      ignores = let
+        inherit (builtins) readFile;
+        inherit (lib) filter hasPrefix splitString;
+        readLines = file: splitString "\n" (readFile file);
+        removeComments = filter (line: line != "" && !(hasPrefix "#" line));
+        getPaths = file: removeComments (readLines file);
+      in
+        getPaths ./default.ignore;
     };
-
-    # There's more
-    extraConfig = {
-      # Makes it a bit more readable
-      blame = {
-        coloring = "repeatedLines";
-        markIgnoredLines = true;
-        markUnblamables = true;
-      };
-
-      # I want `pull --rebase` as a default
-      branch = {
-        autoSetupRebase = "always";
-      };
-
-      checkout = {
-        defaultRemote = "origin";
-      };
-
-      # Shiny colors
-      color = {
-        branch = "auto";
-        diff = "auto";
-        interactive = "auto";
-        status = "auto";
-        ui = "auto";
-      };
-
-      # Pretty much the usual diff colors
-      "color.diff" = {
-        commit = "yellow";
-        frag = "cyan";
-        meta = "yellow";
-        new = "green";
-        old = "red";
-        whitespace = "red reverse";
-      };
-
-      commit = {
-        # Show my changes when writing the message
-        verbose = true;
-        # We will sign using ssh key
-        gpgsign = true;
-      };
-
-      diff = {
-        # Usually leads to better results
-        algorithm = "patience";
-      };
-
-      fetch = {
-        # I don't want hanging references
-        prune = true;
-        pruneTags = true;
-      };
-
-      gpg = {
-        ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-      };
-
-      init = {
-        defaultBranch = "main";
-      };
-
-      merge = {
-        conflictStyle = "zdiff3";
-      };
-
-      pull = {
-        # Avoid useless merge commits
-        rebase = true;
-        ff = "only";
-      };
-
-      push = {
-        default = "simple";
-      };
-
-      rebase = {
-        autoSquash = true;
-        autoStash = true;
-      };
-
-      rerere = {
-        enabled = true;
-      };
-      # Explicitly use ssh key for signing
-      user = {
-        signingkey = "~/.ssh/id_ed25519.pub";
-      };
-
-      url = {
-        "ssh://git@" = {
-          pushInsteadOf = "https://";
-        };
-      };
-    };
-
-    ignores = let
-      inherit (builtins) readFile;
-      inherit (lib) filter hasPrefix splitString;
-      readLines = file: splitString "\n" (readFile file);
-      removeComments = filter (line: line != "" && !(hasPrefix "#" line));
-      getPaths = file: removeComments (readLines file);
-    in
-      getPaths ./default.ignore;
+    lazygit.enable = cfg.lazygit;
   };
 }
