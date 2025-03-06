61b155a992e5b65b31a1257e44cfdbe8709695e9
Joaqim Planstedt <mail@joaqim.xyz>
2025-03-06 09:37:27 +0100

    feat(nixos): Add option `my.hardware.graphics.nvidia`

--- modules/nixos/hardware/graphics/default.nix
+++ modules/nixos/hardware/graphics/default.nix
@@ -10,20 +10,28 @@ in {
     enable = mkEnableOption "graphics configuration";
 
     gpuFlavor = mkOption {
-      type = with types; nullOr (enum ["amd" "intel"]);
+      type = with types; nullOr (enum ["amd" "intel" "nvidia"]);
       default = null;
       example = "intel";
       description = "Which kind of GPU to install driver for";
     };
 
     amd = {
-      enableKernelModule = lib.my.mkDisableOption "Kernel driver module";
+      enableKernelModule = mkEnableOption "Kernel driver module";
 
       amdvlk = lib.mkEnableOption "Use AMDVLK instead of Mesa RADV driver";
     };
 
     intel = {
-      enableKernelModule = lib.my.mkDisableOption "Kernel driver module";
+      enableKernelModule = mkEnableOption "Kernel driver module";
+    };
+
+    nvidia = {
+      enableKernelModule = mkEnableOption "Kernel driver module";
+      # TODO: This might not be correct: https://nixos.wiki/wiki/Nvidia
+      package = mkPackageOption pkgs.nvidiaPackages "nvidia" {
+        default = "stable";
+      };
     };
   };
 
@@ -84,6 +92,38 @@ in {
           intel-vaapi-driver
           libvdpau-va-gl
         ];
+      };
+    })
+
+    # Nvidia GPU
+    (lib.mkIf (cfg.gpuFlavor == "nvidia") {
+      hardware.nvidia = {
+        inherit (cfg.nvidia) package;
+
+        # Modesetting is required.
+        modesetting.enable = true;
+
+        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+        # Enable this if you have graphical corruption issues or application crashes after waking
+        # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+        # of just the bare essentials.
+        powerManagement.enable = false;
+
+        # Fine-grained power management. Turns off GPU when not in use.
+        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+        powerManagement.finegrained = false;
+
+        # Use the NVidia open source kernel module (not to be confused with the
+        # independent third-party "nouveau" open source driver).
+        # Support is limited to the Turing and later architectures. Full list of
+        # supported GPUs is at:
+        # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+        # Only available from driver 515.43.04+
+        open = false;
+
+        # Enable the Nvidia settings menu,
+        # accessible via `nvidia-settings`.
+        nvidiaSettings = true;
       };
     })
   ]);
