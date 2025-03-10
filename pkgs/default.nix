46d90375f88532a9e433cb300d31f94c10c8fdec
Joaqim Planstedt <mail@joaqim.xyz>
2025-03-10 18:44:19 +0100

    feat(pkgs): Use `my.lib.mapModules` to dynamically use `callPackage` for modules in `./pkgs/{package}`

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,29 +1,11 @@
 {
-  self,
-  pkgs,
   flake-inputs,
+  pkgs,
+  self,
 }: let
+  inherit (self.lib.my) mapModules;
   sources = pkgs.callPackage ./sources.nix {};
   callPackage = pkgs.lib.callPackageWith (pkgs // {inherit self sources flake-inputs;});
 in
-  pkgs.lib.makeScope pkgs.newScope (_: {
-    # "Packages" that just contain utility scripts
-    commit-nvfetcher = callPackage ./commit-nvfetcher {};
-    mpv-history-launcher = callPackage ./mpv-history-launcher {};
-
-    # Extensions to existing Applications
-    ## MPV
-    mpv-skipsilence = callPackage ./mpv-skipsilence {};
-    mpv-org-history = callPackage ./mpv-org-history {};
-    ## Firefox Addons
-    chronotube = callPackage ./chronotube {};
-
-    # Plasmoids
-    twitchindicator = callPackage ./twitchindicator {};
-
-    # Proper packages
-    undertaker141 = callPackage ./undertaker141 {};
-
-    # Packages that override existing packages in `nixpkgs`
-    yt-dlp = callPackage ./yt-dlp {};
-  })
+  pkgs.lib.makeScope pkgs.newScope (_:
+    mapModules ./. (file: callPackage file {}))
