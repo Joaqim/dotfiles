6a1206a198df6cc5da3f6978e35676c44edd0612
Joaqim Planstedt <mail@joaqim.xyz>
2025-03-11 19:52:41 +0100

    fix(nixos): Add `mkEnableOption` for `ckb-next` and `liquidctl` in `my.profiles.devices`

--- hosts/nixos/desktop/profiles.nix
+++ hosts/nixos/desktop/profiles.nix
@@ -2,8 +2,12 @@
   my.profiles = {
     # Bluetooth configuration and mpris service
     bluetooth.enable = true;
-    # Mouse and keyboard configuration
-    devices.enable = true;
+    # External devices configuration
+    devices = {
+      enable = true;
+      useCkbNext = true;
+      useLiquidCtl = true;
+    };
     # My custom fcitx5 configuration
     fcitx5.enable = true;
     # Plasma Window Manager
--- modules/nixos/profiles/devices/default.nix
+++ modules/nixos/profiles/devices/default.nix
@@ -1,20 +1,24 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.my.profiles.devices;
 in {
   options.my.profiles.devices = with lib; {
     enable = mkEnableOption "devices profile";
+
+    useCkbNext = mkEnableOption "use ckb-next";
+    useLiquidCtl = mkEnableOption "use liquidctl";
   };
 
   config = lib.mkIf cfg.enable {
     my = {
       hardware = {
-        ckb-next.enable = true;
+        ckb-next.enable = cfg.useCkbNext;
 
-        liquidctl.enable = true;
+        liquidctl.enable = cfg.useLiquidCtl;
       };
       #  Automatically mount external drives configured in fstab
       home.udiskie.enable = true;
