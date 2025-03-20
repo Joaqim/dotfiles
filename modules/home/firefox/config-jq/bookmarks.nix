dc7152d1cfe57475e3f1440b2fdc09a5e578d5b9
Joaqim Planstedt <mail@joaqim.xyz>
2025-03-20 17:39:12 +0100

    chore(firefox): Update `programs.firefox.profiles.<user>.bookmarks` submodule declaration

--- modules/home/firefox/config-jq/bookmarks.nix
+++ modules/home/firefox/config-jq/bookmarks.nix
@@ -1,142 +1,145 @@
-[
-  {
-    name = "Bookmarks";
-    toolbar = true;
-    bookmarks = [
-      # File Management
-      {
-        name = "GitHub";
-        url = "https://github.com/Joaqim";
-        tags = ["git" "github"];
-        keyword = "GitHub";
-      }
-      {
-        name = "Github (dotfiles)";
-        url = "https://github.com/Joaqim/dotfiles";
-        tags = ["dot" "dotfiles"];
-        keyword = "Dotfiles";
-      }
-      {
-        name = "Nix Home Manager Options";
-        url = "https://mipmip.github.io/home-manager-option-search/";
-        tags = ["nix" "nixos" "home" "home manager" "options"];
-        keyword = "Home";
-      }
-      {
-        name = "Syncthing";
-        url = "https://desktop:8384";
-        tags = ["syncthing" "sync" "cloud"];
-        keyword = "Sync";
-      }
-      # Social Media
-      {
-        name = "Gmail";
-        url = "https://mail.google.com/mail/u/0/#inbox";
-        tags = ["gmail" "google" "mail" "gm" "email"];
-        keyword = "Gmail";
-      }
-      {
-        name = "Reddit";
-        url = "https://www.reddit.com/";
-        tags = ["reddit" "social"];
-        keyword = "Reddit";
-      }
-      # Streaming
-      {
-        name = "YouTube ( Subscriptions )";
-        url = "https://www.youtube.com/feed/subscriptions";
-        tags = ["youtube" "you" "tube" "yt"];
-        keyword = "You";
-      }
-      {
-        name = "Dropout";
-        url = "https://www.dropout.tv/new-releases";
-        tags = ["dropout" "drop" "dtv"];
-        keyword = "Op";
-      }
-      # Anime
-      {
-        name = "Anilist";
-        url = "https://www.anilist.co/home";
-        tags = ["anilist" "ani" "al"];
-        keyword = "al";
-      }
-      # Shopping
-      {
-        name = "Amazon";
-        url = "https://www.amazon.com/";
-        tags = ["amazon" "shopping" "supply"];
-        keyword = "Amazon";
-      }
-      # Gaming
-      {
-        name = "Chess.com";
-        url = "https://www.chess.com/home";
-        tags = ["chess"];
-        keyword = "Chess";
-      }
-      {
-        name = "Lichess";
-        url = "https://lichess.org/";
-        tags = ["lichess" "chess"];
-        keyword = "Li";
-      }
-      {
-        name = "ProtonDB";
-        url = "https://www.protondb.com/";
-        tags = ["steam" "db"];
-        keyword = "DB";
-      }
-      {
-        name = "SteamDB";
-        url = "https://steamdb.info/";
-        tags = ["steamdb" "steam" "db"];
-        keyword = "SteamDB";
-      }
-      {
-        name = "Sunshine";
-        url = "http://node:47990";
-        tags = ["sunshine"];
-        keyword = "SN";
-      }
-      # Work
-      {
-        name = "Slack";
-        url = "https://app.slack.com/client/T0VGY738S/D01S0DACFCK";
-        tags = ["slack"];
-        keyword = "Sl";
-      }
-      # Misc
-      {
-        name = "ChatGPT";
-        url = "https://chatgpt.com/";
-        tags = ["chat" "chatgpt" "gpt"];
-        keyword = "Chat";
-      }
-      {
-        name = "Google Maps";
-        url = "https://www.maps.google.com/";
-        tags = ["maps" "google"];
-        keyword = "Map";
-      }
-      {
-        name = "Speedtest";
-        url = "https://www.speedtest.net/";
-        tags = ["speedtest" "speed" "test"];
-        keyword = "Speed";
-      }
-      {
-        name = "TinEye";
-        url = "https://tineye.com/";
-        tags = ["tineye" "tin" "eye"];
-        keyword = "Tin";
-      }
-      {
-        name = "Kaleidosync";
-        url = "https://www.kaleidosync.com/";
-        tags = ["kaleidosync" "kaleido" "sync" "visualizer"];
-        keyword = "Kaleido";
-      }
-    ];
-  }
-]
+{
+  force = true;
+  settings = [
+    {
+      name = "Bookmarks";
+      toolbar = true;
+      bookmarks = [
+        # File Management
+        {
+          name = "GitHub";
+          url = "https://github.com/Joaqim";
+          tags = ["git" "github"];
+          keyword = "GitHub";
+        }
+        {
+          name = "Github (dotfiles)";
+          url = "https://github.com/Joaqim/dotfiles";
+          tags = ["dot" "dotfiles"];
+          keyword = "Dotfiles";
+        }
+        {
+          name = "Nix Home Manager Options";
+          url = "https://mipmip.github.io/home-manager-option-search/";
+          tags = ["nix" "nixos" "home" "home manager" "options"];
+          keyword = "Home";
+        }
+        {
+          name = "Syncthing";
+          url = "https://desktop:8384";
+          tags = ["syncthing" "sync" "cloud"];
+          keyword = "Sync";
+        }
+        # Social Media
+        {
+          name = "Gmail";
+          url = "https://mail.google.com/mail/u/0/#inbox";
+          tags = ["gmail" "google" "mail" "gm" "email"];
+          keyword = "Gmail";
+        }
+        {
+          name = "Reddit";
+          url = "https://www.reddit.com/";
+          tags = ["reddit" "social"];
+          keyword = "Reddit";
+        }
+        # Streaming
+        {
+          name = "YouTube ( Subscriptions )";
+          url = "https://www.youtube.com/feed/subscriptions";
+          tags = ["youtube" "you" "tube" "yt"];
+          keyword = "You";
+        }
+        {
+          name = "Dropout";
+          url = "https://www.dropout.tv/new-releases";
+          tags = ["dropout" "drop" "dtv"];
+          keyword = "Op";
+        }
+        # Anime
+        {
+          name = "Anilist";
+          url = "https://www.anilist.co/home";
+          tags = ["anilist" "ani" "al"];
+          keyword = "al";
+        }
+        # Shopping
+        {
+          name = "Amazon";
+          url = "https://www.amazon.com/";
+          tags = ["amazon" "shopping" "supply"];
+          keyword = "Amazon";
+        }
+        # Gaming
+        {
+          name = "Chess.com";
+          url = "https://www.chess.com/home";
+          tags = ["chess"];
+          keyword = "Chess";
+        }
+        {
+          name = "Lichess";
+          url = "https://lichess.org/";
+          tags = ["lichess" "chess"];
+          keyword = "Li";
+        }
+        {
+          name = "ProtonDB";
+          url = "https://www.protondb.com/";
+          tags = ["steam" "db"];
+          keyword = "DB";
+        }
+        {
+          name = "SteamDB";
+          url = "https://steamdb.info/";
+          tags = ["steamdb" "steam" "db"];
+          keyword = "SteamDB";
+        }
+        {
+          name = "Sunshine";
+          url = "http://node:47990";
+          tags = ["sunshine"];
+          keyword = "SN";
+        }
+        # Work
+        {
+          name = "Slack";
+          url = "https://app.slack.com/client/T0VGY738S/D01S0DACFCK";
+          tags = ["slack"];
+          keyword = "Sl";
+        }
+        # Misc
+        {
+          name = "ChatGPT";
+          url = "https://chatgpt.com/";
+          tags = ["chat" "chatgpt" "gpt"];
+          keyword = "Chat";
+        }
+        {
+          name = "Google Maps";
+          url = "https://www.maps.google.com/";
+          tags = ["maps" "google"];
+          keyword = "Map";
+        }
+        {
+          name = "Speedtest";
+          url = "https://www.speedtest.net/";
+          tags = ["speedtest" "speed" "test"];
+          keyword = "Speed";
+        }
+        {
+          name = "TinEye";
+          url = "https://tineye.com/";
+          tags = ["tineye" "tin" "eye"];
+          keyword = "Tin";
+        }
+        {
+          name = "Kaleidosync";
+          url = "https://www.kaleidosync.com/";
+          tags = ["kaleidosync" "kaleido" "sync" "visualizer"];
+          keyword = "Kaleido";
+        }
+      ];
+    }
+  ];
+}
