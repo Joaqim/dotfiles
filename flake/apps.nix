84bc02ac1839fcac29fa839506f7d510faee60cf
Joaqim Planstedt <mail@joaqim.xyz>
2025-03-30 00:20:40 +0100

    feat(flake): apps.nix: Refactor and add `dry-activate` utility for nixos-rebuild & home-manager switch

--- flake/apps.nix
+++ flake/apps.nix
@@ -1,14 +1,47 @@
-{inputs, ...}: {
-  perSystem = {self', ...}: {
-    apps = {
-      commit-nvfetcher = {
+# [1]: https://github.com/kachick/dotfiles/blob/main/scripts/dry.bash
+{lib, ...}: {
+  perSystem = {
+    self',
+    inputs',
+    ...
+  }: {
+    apps = let
+      inherit (inputs'.nixpkgs.legacyPackages) writeShellScript;
+      commit-nvfetcher = lib.getExe self'.packages.commit-nvfetcher;
+      home-manager = lib.getExe inputs'.home-manager.packages.home-manager;
+
+      mkApp = program: {
+        inherit program;
         type = "app";
-        program = toString (
-          inputs.nixpkgs.legacyPackages.x86_64-linux.writeShellScript "commit-nvfetcher" ''
-            ${inputs.self.packages.x86_64-linux.commit-nvfetcher}/bin/commit-nvfetcher -k /tmp/github-key.toml
-          ''
-        );
       };
+    in {
+      commit-nvfetcher = mkApp (toString (
+        writeShellScript "commit-nvfetcher" ''
+          ${commit-nvfetcher} -k /tmp/github-key.toml
+        ''
+      ));
+
+      home-manager = mkApp home-manager;
+      dry-activate = mkApp (toString (
+        # [1]:
+        writeShellScript "dry-activate"
+        ''
+          set -exo pipefail
+
+          if [ -f '/etc/NIXOS' ]; then
+          	# Prefer `build` rather than `test` and `dry-reactivate` for the speed and not requiring root permissions
+          	nixos-rebuild build --flake ".#$(hostname)" --show-trace
+          fi
+
+          _HOME_CONF=".#$USER"
+          if [ ! -z "$HM_HOST_SLUG" ]; then
+            _HOME_CONF+="@$HOSTNAME"
+          fi
+
+          nix run '.#home-manager' -- switch --dry-run -b backup --show-trace --flake $_HOME_CONF
+        ''
+      ));
+
       default = self'.apps.commit-nvfetcher;
     };
   };
