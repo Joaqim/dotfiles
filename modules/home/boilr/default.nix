40a61901b0fa0c8e348976833e90c9ad5d2da716
Joaqim Planstedt <mail@joaqim.xyz>
2025-04-01 21:01:54 +0200

    feat(homes): Add `boilr` config with secrets and `no ui` startup service

--- modules/home/boilr/default.nix
+++ modules/home/boilr/default.nix
@@ -8,14 +8,29 @@
 in {
   options.my.home.boilr = with lib; {
     enable = mkEnableOption "boilr service to automatically add artwork to non-Steam shortcuts";
+
+    runOnStartup = mkEnableOption "add artwork on startup using systemd service";
   };
   config = lib.mkIf cfg.enable {
-    # Make boilr executable available
-    home.packages = [pkgs.boilr];
-    systemd.user.services.boilr = {
+    home = {
+      packages = [
+        pkgs.boilr
+      ];
+    };
+    sops.templates."boilr-config.toml" = {
+      content =
+        builtins.readFile ./config.toml
+        + ''
+
+          auth_key = "${config.sops.placeholder."steamgrid_db_auth_key"}"
+        '';
+      path = "${config.home.homeDirectory}/.config/boilr/config.toml";
+    };
+
+    systemd.user.services.boilr = lib.mkIf cfg.runOnStartup {
       Unit = {
         Description = "Run Boilr without gui to automatically add artwork to non-Steam shortcuts.";
-        After = ["NetworkManager.target"];
+        After = ["NetworkManager.target" "sops-nix.service"];
       };
       Service = {
         ExecStart = "${lib.getExe pkgs.boilr} --no-ui";
