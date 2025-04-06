818e431af9e78c2d603a7b97e18b508a0b313dfd
Joaqim Planstedt <mail@joaqim.xyz>
2025-04-06 14:56:15 +0200

    chore(nixos): deck: Enable `my.system.nix.cache.selfHosted`

--- hosts/nixos/deck/system.nix
+++ hosts/nixos/deck/system.nix
@@ -2,14 +2,17 @@
   inherit (config.sops) secrets;
   inherit (config.my.user) name;
 in {
-  my.system.users = rec {
-    enable = true;
-    defaultPasswordFile =
-      if builtins.hasAttr "user_hashed_password/${name}" secrets
-      then secrets."user_hashed_password/${name}".path
-      else null;
+  my.system = {
+    nix.cache.selfHosted = true;
+    users = rec {
+      enable = true;
+      defaultPasswordFile =
+        if builtins.hasAttr "user_hashed_password/${name}" secrets
+        then secrets."user_hashed_password/${name}".path
+        else null;
 
-    enableRootAccount = true;
-    rootPasswordFile = defaultPasswordFile;
+      enableRootAccount = true;
+      rootPasswordFile = defaultPasswordFile;
+    };
   };
 }
