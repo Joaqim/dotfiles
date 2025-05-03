75b8797ee2c54c5ceb4557f4bf87db41b04f11b3
Joaqim Planstedt <mail@joaqim.xyz>
2025-05-03 15:59:40 +0200

    feat(nixos): hardware.ckb-next: Add default enabled autostart item for `ckb-next`

--- modules/nixos/hardware/ckb-next/default.nix
+++ modules/nixos/hardware/ckb-next/default.nix
@@ -2,15 +2,32 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.my.hardware.ckb-next;
 in {
   options.my.hardware.ckb-next = with lib; {
     enable = mkEnableOption "CKB Next keyboard/mouse drivers and user configuration";
+
+    enableAutoStart = my.mkDisableOption "enable autostart";
   };
 
   config = lib.mkIf cfg.enable {
     hardware.ckb-next.enable = true;
+    environment.systemPackages = [
+      (pkgs.makeAutostartItem rec {
+        name = "ckb-next";
+        package = pkgs.makeDesktopItem {
+          inherit name;
+          desktopName = "CKB Next";
+          exec = "ckb-next --background";
+          icon = "ckb-next";
+          extraConfig = {
+            OnlyShowIn = "KDE";
+          };
+        };
+      })
+    ];
   };
 }
