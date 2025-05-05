31b9d3d064136e515af9317c48aa8a7b038c5ee7
Joaqim Planstedt <mail@joaqim.xyz>
2025-05-05 03:55:45 +0200

    bump(flake): flake.lock: Update flake inputs
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/50bb714a8259b0c29b6c3429099a3b837771dab4?narHash=sha256-GJEUJpZLkczMN6HXD0wdFX6KyDbvZe3v5orUhqEfK6w%3D' (2025-04-27)
      → 'github:nix-community/home-manager/1a1793f6d940d22c6e49753548c5b6cb7dc5545d?narHash=sha256-m3ai7LLFYsymMK0uVywCceWfUhP0k3CALyFOfcJACqE%3D' (2025-05-04)

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       inputs.systems.follows = "systems";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/50bb714a8259b0c29b6c3429099a3b837771dab4";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     impermanence = {
