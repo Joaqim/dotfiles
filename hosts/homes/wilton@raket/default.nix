f9d0f5069cb7bc2c23816c155264d7576c8fe729
Joaqim Planstedt <mail@joaqim.xyz>
2025-07-07 16:02:21 +0200

    fix(homes): Attempt to fix sops-nix not working for wilton@raket

--- hosts/homes/wilton@raket/default.nix
+++ hosts/homes/wilton@raket/default.nix
@@ -1,5 +1,24 @@
-{
+{pkgs, ...}: {
+  home = rec {
+    username = "wilton";
+    homeDirectory = "/home/${username}";
+  };
+
   my.home = {
+    packages.additionalPackages = builtins.attrValues {
+      inherit
+        (pkgs)
+        # TODO: Is this still needed?
+        # KDE complains, even if nvidia doesn't seem to provide power profiles ?
+        power-profiles-daemon
+        discord
+        ;
+      inherit
+        (pkgs.nur.repos.nltch)
+        spotify-adblock
+        ;
+    };
+    secrets.enable = false; # Disable for now
     atuin.enable = false;
     boilr.enable = false;
     bottom.enable = false;
