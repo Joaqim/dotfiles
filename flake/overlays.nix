0ca6dd4282a7580fbc8d6eecee5dd8382e850eab
Joaqim Planstedt <mail@joaqim.xyz>
2025-11-03 16:43:22 +0100

    chore(flake): Minor syntax changes

--- flake/overlays.nix
+++ flake/overlays.nix
@@ -6,13 +6,12 @@
 }:
 let
   default-overlays = import "${self}/overlays";
-
   additional-overlays = {
     # Expose my expanded library
     lib = _final: _prev: { inherit (self) lib; };
 
-    # Expose my custom packages
     pkgs = _final: prev: {
+      # Expose my custom packages
       jqpkgs = lib.recurseIntoAttrs (
         import "${self}/pkgs" {
           inherit self;
@@ -20,13 +19,8 @@ let
           flake-inputs = inputs;
         }
       );
-      /*
-        https://github.com/nix-community/nur-combined/blob/fb903d2993b7cd6961b7d64b97c9d3938badc5ca/repos/aasg/lib/attrsets.nix#L53
-        recurseIntoAttrs :: set -> set
-
-        Polyfill of pkgs.recurseIntoAttrs for when it's accessed from lib
-      */
-      recurseIntoAttrs = lib.recurseIntoAttrs or (attrs: attrs // { recurseForDerivations = true; });
+      # Polyfill pkgs.recurseIntoAttrs for when it's not accessed from lib
+      recurseIntoAttrs = lib.recurseIntoAttrs;
     };
   };
 in
