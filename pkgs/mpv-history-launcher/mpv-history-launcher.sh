a71af2d8c0b6aa64520f9f3c11e8545d047543ee
Joaqim Planstedt <mail@joaqim.xyz>
2026-02-19 17:37:12 +0100

    feat(pkgs): mpv-history-launcher: Update simple kdialog to play entries from my custom mpv org history

--- pkgs/mpv-history-launcher/mpv-history-launcher.sh
+++ pkgs/mpv-history-launcher/mpv-history-launcher.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 _HISTORY_FILE="/home/jq/Documents/org/mpv-history.org"
 _LINK_CAPTURE_REGEX="^\*\*\* (.*)(\W:)(.*):<(.*)>:<(.*)>:<(.*)>:.*$"
-[ -z $_HISTORY_FILE ] && echo "Missing history file at: ${_HISTORY_FILE}" && exit 1
+[ -z "$_HISTORY_FILE" ] && echo "Missing history file at: $_HISTORY_FILE" && exit 1
 
-history=$(tail "${_HISTORY_FILE}" -n 50)
+mapfile -t history <<< "$(tail "$_HISTORY_FILE" -n 150)"
 options=()
-while IFS= read -r line; do
-    if [[ "$line" =~ $_LINK_CAPTURE_REGEX ]]; then
+for (( i = ${#history[@]}; i--; )); do
+    if [[ "${history[i]}" =~ $_LINK_CAPTURE_REGEX ]]; then
         current_time="${BASH_REMATCH[5]}"
         runtime="${BASH_REMATCH[6]}"
         current_time_in_sec=$(date -d "$current_time" +%s)
@@ -19,11 +19,9 @@ while IFS= read -r line; do
         options+=("${BASH_REMATCH[1]}")
         options+=("${BASH_REMATCH[1]} - ${BASH_REMATCH[3]} - $current_time/$runtime")
     fi
-done <<< "$history"
-# Reverse array
-#options=$(printf '%s\n' "${options[@]}" | tac | tr '\n' ' '; echo)
+done
 
-result=$(kdialog --geometry 1200x900+300+300 --menu dialog "${options[@]}")
+result=$(kdialog --geometry 1200x900+300+300 --menu "Choose entry to play with MPV" "${options[@]}")
 
 if [ -z "$result" ]; then
     exit 0
