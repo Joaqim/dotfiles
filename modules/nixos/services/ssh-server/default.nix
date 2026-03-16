097824dcf3f1aeec1ce83cfa94f52c19df58fdd9
Joaqim Planstedt <mail@joaqim.xyz>
2026-03-16 14:04:21 +0100

    chore(nixos): my.services.ssh-server: Add `github.com` to known hosts

--- modules/nixos/services/ssh-server/default.nix
+++ modules/nixos/services/ssh-server/default.nix
@@ -23,8 +23,14 @@ in
         PasswordAuthentication = false;
       };
     };
+    programs = {
+      # Opens the relevant UDP ports.
+      mosh.enable = true;
 
-    # Opens the relevant UDP ports.
-    programs.mosh.enable = true;
+      ssh.knownHosts = {
+        "github.com".publicKey =
+          "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==";
+      };
+    };
   };
 }
