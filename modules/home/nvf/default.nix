fe5f775b63b07925025e90249c55c5b03306715a
Joaqim Planstedt <mail@joaqim.xyz>
2026-03-19 10:57:17 +0100

    fix(home): my.home.nvf: Manually add missing spellcheck dictionaries

--- modules/home/nvf/default.nix
+++ modules/home/nvf/default.nix
@@ -29,9 +29,22 @@ in
               "en"
               "sv"
             ];
-            programmingWordlist.enable = true;
+            #programmingWordlist.enable = true;
           };
         };
+      };
+    };
+    xdg.configFile = {
+      # https://neovim.io/doc/user/plugins.html#spellfile.lua
+      # TODO: Consider using a unified spell root directory and adding to additionalRuntimePaths:  https://nvf.notashelf.dev/options.html#option-vim-additionalRuntimePaths
+      "nvf/spell/en.utf-8.spl".source = builtins.fetchurl {
+        url = "https://github.com/vim/vim/raw/refs/heads/master/runtime/spell/en.utf-8.spl";
+        sha256 = "0w1h9lw2c52is553r8yh5qzyc9dbbraa57w9q0r9v8xn974vvjpy";
+      };
+      # Might require nix-prefetch-url ?
+      "nvf/spell/sv.utf-8.spl".source = builtins.fetchurl {
+        url = "ftp://ftp.fu-berlin.de/unix/editors/vim/runtime/spell/sv.utf-8.spl";
+        sha256 = "0lmbfb6cw38dzic388m9gc5ajx30j9npdz48acfs4v3nl52jivbk";
       };
     };
   };
