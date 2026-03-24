9af16506f732e7b32f361511f2a6fa20b5e0547f
Joaqim Planstedt <mail@joaqim.xyz>
2026-03-24 20:03:21 +0100

    chore(home): my.home.terminal: Add some `kitty` configuration defaults

--- modules/home/terminal/kitty/default.nix
+++ modules/home/terminal/kitty/default.nix
@@ -10,6 +10,13 @@ in
   config = lib.mkIf (cfg.program == "kitty") {
     programs.kitty = {
       enable = true;
+      enableGitIntegration = true;
+      settings = {
+        scrollback_lines = 10000;
+        enable_audio_bell = false;
+
+        wayland_enable_ime = true;
+      };
     };
   };
 }
