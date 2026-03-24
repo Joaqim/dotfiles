afa72e6a219d265e1358c85fbb7cf36d25f2b61f
Joaqim Planstedt <mail@joaqim.xyz>
2026-03-24 20:02:31 +0100

    feat(nixos): my.profiles.fcitx5: Enable custom experimental addon `jqwerty` for support of location based qwerty modified keymap
    fix(deps): flake.lock: Update input `jqpkgs` to properly include new pkg: `fcitx5-jqwerty`

--- modules/nixos/profiles/fcitx5/default.nix
+++ modules/nixos/profiles/fcitx5/default.nix
@@ -1,6 +1,8 @@
 {
   config,
   lib,
+  pkgs,
+  inputs,
   ...
 }:
 let
@@ -10,42 +12,28 @@ in
   options.my.profiles.fcitx5 = with lib; {
     enable = mkEnableOption "fcitx5 configuration";
   };
+
   config = lib.mkIf cfg.enable {
     i18n.inputMethod = {
       enable = true;
       type = "fcitx5";
       fcitx5 = {
-        ignoreUserConfig = false;
+        addons = [
+          inputs.jqpkgs.packages.${pkgs.stdenv.hostPlatform.system}.fcitx5-jqwerty
+        ];
+
+        ignoreUserConfig = true;
+
         settings = {
-          globalOptions = builtins.fromTOML (builtins.readFile ./fcitx5-options.toml);
-          inputMethod = {
-            "Groups/0" = {
-              "Name" = "Default";
-              "Default Layout" = "us";
-              "DefaultIM" = "keyboard-us";
-            };
-            "Groups/0/Items/0" = {
-              "Name" = "keyboard-us-dvp";
-              "Layout" = "";
-            };
-            "Groups/0/Items/1" = {
-              "Name" = "keyboard-us";
-              "Layout" = "";
-            };
-            "GroupOrder" = {
-              "0" = "Default";
+          globalOptions = {
+            Behaviour = {
+              CustomXkbOption = "caps:escape";
+              EnableAddons = "jqwerty";
             };
           };
         };
       };
     };
     my.home.gtk.useFcitx5 = true;
-    environment = {
-      variables = {
-        GTK_IM_MODULE = "fcitx";
-        XMODIFIERS = "@im=fcitx";
-        QT_IM_MODULES = "wayland;fcitx;ibus";
-      };
-    };
   };
 }
